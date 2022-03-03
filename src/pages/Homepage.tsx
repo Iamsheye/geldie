a4885c5e1bf9af353db1d5f333db439d43ee4faf
Iamsheye <seyepelumi@gmail.com>
2022-03-03 13:46:14 +0100

    fixed tailwind issue

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,10 @@
 const Homepage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-blue-200">
+    <section className="wrapper">
+      <h1 className="text-3xl font-bold underline text-blue-700">
         PROJECT GELDIE HOME PAGE
       </h1>
-      <div className="flex items-center justify-between">
-        <p>Hii</p>
-        <p>Heeyyy</p>
-        <p>Hellloooo</p>
-      </div>
-    </div>
+    </section>
   );
 };
 
