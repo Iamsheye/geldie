69bc6dca9dba1f15ace83ac363ac85ce2417d509
Iamsheye <seyepelumi@gmail.com>
2022-03-03 04:00:25 +0100

    project init

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,16 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-200">
+        PROJECT GELDIE HOME PAGE
+      </h1>
+      <div className="flex items-center justify-between">
+        <p>Hii</p>
+        <p>Heeyyy</p>
+        <p>Hellloooo</p>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
