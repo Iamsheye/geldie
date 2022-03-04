33db7079f2fbc69137f9a93f668d8453221a1e20
Iamsheye <seyepelumi@gmail.com>
2022-03-04 02:26:43 +0100

    pushed header

---
+++ src/components/Header.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
+import { useMoralis } from "react-moralis";
+import { Link } from "react-router-dom";
+import ethereum from "../eth";
+
+interface IHeader {
+  userAddress: string;
+  setUserAddress: (value: React.SetStateAction<string>) => void;
+}
+
+const Header = ({ userAddress, setUserAddress }: IHeader) => {
+  const {
+    user,
+    authenticate,
+    isAuthenticated,
+    isAuthenticating,
+    authError,
+    logout,
+  } = useMoralis();
+
+  if (authError) {
+    toast.error(authError.message);
+  }
+
+  useEffect(() => {
+    setUserAddress(ethereum.selectedAddress);
+  }, [user]);
+
+  return (
+    <header className="py-8 ">
+      <nav className="flex items-center justify-between">
+        <div>
+          <Link to="/">
+            <p className="text-5xl text-[#ffed8e] font-['Pacifico']">geldie</p>
+          </Link>
+        </div>
+
+        <div>
+          {isAuthenticated ? (
+            <button
+              disabled
+              className="text-white bg-slate-800 rounded-full py-2.5 px-5"
+            >
+              {`${userAddress.split("").splice(0, 3).join("")}...${userAddress
+                .split("")
+                .splice(-5)
+                .join("")}`}{" "}
+              <span
+                onClick={() => logout()}
+                className="ml-3 cursor-pointer text-[0.85rem]"
+              >
+                ❌
+              </span>
+            </button>
+          ) : (
+            <button
+              onClick={() =>
+                authenticate({
+                  signingMessage: "Project Geldie Authentication",
+                })
+              }
+              className="text-white bg-gradient-to-tr from-green-600 to-sky-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-sky-300 font-medium rounded-full text-sm px-5 py-2.5"
+            >
+              {isAuthenticating ? "Connecting Wallet.." : "Connect Wallet 💫"}
+            </button>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,60 +4,14 @@ import { useEffect, useState } from "react";
 import Moralis from "moralis";
 
 import ethereum from "../eth";
+import Header from "../components/Header";
 
 const Homepage = () => {
   const [userAddress, setUserAddress] = useState<string>("");
 
-  const {
-    user,
-    authenticate,
-    isAuthenticated,
-    isAuthenticating,
-    authError,
-    logout,
-  } = useMoralis();
-
-  if (authError) {
-    toast.error(authError.message);
-  }
-
-  useEffect(() => {
-    setUserAddress(ethereum.selectedAddress);
-  }, [user]);
-
   return (
-    <section className="wrapper pt-14">
-      {/* <h1 className="text-3xl font-bold underline text-blue-700">
-        PROJECT GELDIE HOME PAGE
-      </h1> */}
-      {isAuthenticated ? (
-        <div className="flex items-center justify-end">
-          <button className="relative inline-flex items-center justify-center px-1 py-1 overflow-hidden text-sm font-medium text-gray-900 rounded-l-full group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800">
-            <span className="relative p-1.5 pl-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-l-full group-hover:bg-opacity-0">
-              {`${userAddress.split("").splice(0, 6).join("")}...${userAddress
-                .split("")
-                .splice(-5)
-                .join("")}`}
-            </span>
-          </button>
-
-          <button
-            onClick={() => logout()}
-            className="text-white bg-gradient-to-tr from-red-500 to-amber-600 hover:bg-gradient-to-bl focus:ring-4 focus:ring-red-400 font-medium rounded-r-full text-sm px-5 py-2.5 text-center"
-          >
-            Disconnect Wallet
-          </button>
-        </div>
-      ) : (
-        <button
-          onClick={() =>
-            authenticate({ signingMessage: "Project Geldie Authentication" })
-          }
-          className="text-white bg-gradient-to-tr from-green-600 to-sky-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-sky-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        >
-          {isAuthenticating ? "Connecting Wallet..." : "Connect Wallet"}
-        </button>
-      )}
+    <section className="wrapper">
+      <Header userAddress={userAddress} setUserAddress={setUserAddress} />
     </section>
   );
 };
